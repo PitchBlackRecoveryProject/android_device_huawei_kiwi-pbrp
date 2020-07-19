220cd45a8d7a9a160ae2d39fa5bfc74eda3eb09d
HARSH <iamhp2k@gmail.com>
2020-07-19 18:34:11 +0530

    kiwi: remove gsm makefile

--- omni_kiwi.mk
+++ omni_kiwi.mk
@@ -12,9 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-## Specify phone tech before including full_phone
-$(call inherit-product, vendor/omni/config/gsm.mk)
-
 $(call inherit-product, device/huawei/kiwi/full_kiwi.mk)
 
 # Inherit some common Omni stuff.
